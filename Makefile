4cd03932e331a5382591511035e58c5d653ca3fe
Lester <lester.crespo.es@gmail.com>
2014-12-12 19:52:50 +0000

    Started queue module

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@
 #one module multiple objects
 #module-objs := readmap.o
 
-obj-m := module.o 
+#obj-m := module.o 
+obj-m := queue.o 
 
 #multiple modules
 #obj-m := module.o readmap.o
@@ -17,3 +18,12 @@ obj-m := module.o
 #	$(CC) $(MODCFLAGS) -c module.c
 
 #all : module.o
+
+v := uname -r
+# build for the current host
+host : module
+
+module:
+
+	echo `uname -r`
+	make -C /usr/src/linux-headers-`uname -r` M=${M}  KCONFIG_CONFIG=/mnt/data/users/lester/projects/linux/i5.config modules
--- queue.c
+++ queue.c
@@ -21,8 +21,76 @@
  * to find last used page get remaining and walk through all pages until 0
  */
 
+/// kernel headers
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/miscdevice.h>
+#include <linux/debugfs.h>
+#include <linux/mm.h>  // mmap related stuff
+#include <linux/slab.h>
 
 // page list structure
 
+// map function
+static int mmap(struct file *fd, struct vm_area_struct *vma)
+{
+    struct page *page;
+    struct mmap_info *info;
+    return 0;
+}
 
+// file operations for misc device
+static struct file_operations fops_sys = { //
+        .mmap = mmap //
+
+        };
+// misc device resgistration
+static struct miscdevice misc = { //
+        .name = "queue", //
+        .fops = &fops_sys, //
+        };
+
+static unsigned done = 0;  // how far the module has been initialize
+
+static void cleanup(void)
+{
+    int r;
+    if (done > 0)
+    {
+        r = misc_deregister(&misc);
+        if (r != 0)
+        {
+            printk("failed to deregister misc device %s - code %d\n", misc.name, r);
+        }
+    }
+}
+
+/**
+ * Module initialization routine
+ * @return non zero means module fail
+ */
+static int init(void)
+{
+    int r;		// return code of linux functions
+    for (;;)
+    {
+        // register device as miscelanious in sys/devices/misc
+        r = misc_register(&misc);
+        if (r != 0)
+        {
+            printk("failed to register misc device %s - code %d\n", misc.name, r);
+        }
+        done++;
+        return 0;
+    }
+    cleanup();
+    return done;
+}
+
+module_init(init);
+module_exit(cleanup);
 
