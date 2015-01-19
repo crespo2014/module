59b6b29c97b582449328e0de693542610aace510
lester <lester@debian.ie>
2015-01-19 15:49:34 +0000

    added arm tsc asm source code

--- tickCount.c
+++ tickCount.c
@@ -52,6 +52,41 @@ static inline uint64_t readJiffies_ms(void)
     return jiffies * 1000 / HZ;
 }
 
+static inline unsigned int arm_getTSC(void)
+{
+    unsigned int value;
+    // Read CCNT Register
+    asm volatile ("MRC p15, 0, %0, c9, c13, 0\t\n": "=r"(value));
+    return value;
+}
+
+static inline void arm_init_TSC(int do_reset, int enable_divider)
+{
+    // in general enable all counters (including cycle counter)
+    int value = 1;
+
+    // peform reset:
+    if (do_reset)
+    {
+        value |= 2;     // reset all counters to zero.
+        value |= 4;     // reset cycle counter to zero.
+    }
+
+    if (enable_divider)
+        value |= 8;     // enable "by 64" divider for CCNT.
+
+    value |= 16;
+
+    // program the performance-counter control-register:
+    asm volatile ("MCR p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));
+
+    // enable all counters:
+    asm volatile ("MCR p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));
+
+    // clear overflows:
+    asm volatile ("MCR p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
+}
+
 static int sys_open(struct inode * i, struct file * f)
 {
     struct file_data* pd = kmalloc(sizeof(*pd), GFP_KERNEL);
@@ -139,6 +174,7 @@ static int init(void)
             break;
         }
         done++;
+        //arm_init_TSC(1,1);
         return 0;
     }
     cleanup();
