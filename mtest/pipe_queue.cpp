7c0d405a2d6a3c77b43cb084a6029857ccbf451d
Lester <lester.crespo.es@gmail.com>
2015-03-11 19:50:37 +0000

    complex io operations test

--- mtest/pipe_queue.cpp
+++ mtest/pipe_queue.cpp
@@ -36,7 +36,7 @@ TEST(PipeQueue, fullTest)
 		Pipe p(std::nothrow);
 		CHECK_TRUE(p);
 		uint8_t dt[10];
-		uint8_t rdt[10];
+		uint8_t rdt[50];
 		for (uint8_t i = 0; i < 10; ++i)
 			dt[i] = i;
 		CHECK(p.write(dt,10,std::nothrow) == 10);
@@ -45,24 +45,30 @@ TEST(PipeQueue, fullTest)
 		for (uint8_t i = 0; i < 10; ++i)
 			CHECK(rdt[i] == i);
 		// test pipe for complex read write
+		struct iovec vect[4] = { {dt,5},{dt+4,6},{dt+6,4},{dt+7,3} };
 
+		CHECK(p.writev(vect,4,std::nothrow) == 18);
+
+		// try to read more
+		CHECK(p.read(rdt,30,std::nothrow) == 18);
+
+		for (uint8_t i = 0; i < 5; ++i)
+			CHECK(rdt[i] == i);
+		for (uint8_t i = 4; i < 10; ++i)
+			CHECK(rdt[i+5-4] == i);
+		for (uint8_t i = 6; i < 10; ++i)
+			CHECK(rdt[i+5+6-6] == i);
+		for (uint8_t i = 7; i < 10; ++i)
+			CHECK(rdt[i+5+6+4-7] == i);
 
 		CHECK_TRUE(p.shutdownRead(std::nothrow));
 		CHECK_FALSE(p);
 		CHECK_TRUE(p.shutdownWrite(std::nothrow));
 		CHECK_FALSE(p);
 	}
+}
 
-
-//	char *str0 = "hello ";
-//	char *str1 = "world\n";
-//	struct iovec iov[2];
-//	ssize_t nwritten;
-//
-//	iov[0].iov_base = str0;
-//	iov[0].iov_len = strlen(str0);
-//	iov[1].iov_base = str1;
-//	iov[1].iov_len = strlen(str1);
-//
-//	nwritten = writev(STDOUT_FILENO, iov, 2);
+TEST(PipeQueue, tcpSock)
+{
+}
 }
